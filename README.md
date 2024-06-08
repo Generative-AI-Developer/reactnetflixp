7b6e95e338511023d3f0b7d3733e8cbfcd2c9df0
Generative-AI-Developer <163644637+Generative-AI-Developer@users.noreply.github.com>
2024-06-08 14:25:32 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# reactnetflixp
+ reactnetflixp
---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+const App = () => {
+  return (
+    <div>
+     <NetflexSeries/>
+     <NetflexSeries/>
+     <NetflexSeries/>
+     <NetflexSeries/>
+
+    </div>
+  )
+}
+
+const NetflexSeries = ()=>{
+  return(
+    <>
+    <div>
+    <img src="bg.png" alt="" width="30%" height="30%"/>
+  </div>
+  <h2>Name: Quen of Tears</h2>
+  <h3>Rating: 8.2</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque voluptates mollitia quis quidem dicta sapiente sit aliquid consequatur id, fugit assumenda quaerat tenetur, repellat ipsum, deleniti quod eligendi enim.</p>
+  </>
+  )
+
+
+}
+
+export default App
