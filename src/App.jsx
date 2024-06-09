281027399ff465b5c9df12c20bf68277c6948e68
Generative-AI-Developer <163644637+Generative-AI-Developer@users.noreply.github.com>
2024-06-09 11:52:41 +0500

    conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,18 @@ const NetflexSeries = ()=>{
   const rating = "8.3"
   const summary = "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eaque voluptates mollitia quis quidem dicta sapiente sit aliquid consequatur id, fugit assumenda quaerat tenetur, repellat ipsum, deleniti quod eligendi enim."
 
+  let age = 15;
+
+  // // 2nd Method
+  // let canWatch = "Not Availble"
+  // if(age>=18) canWatch = "Watch Now"
+
+  // 3rd Mothod
+  const canWatch = ()=>{
+    if(age>=18) return "Watch Now";
+    return "Not Available"
+
+  }
 const returnGenera = ()=>{
   const genera = "RomCom"
   return genera
@@ -31,6 +43,18 @@ const returnGenera = ()=>{
   <h3>Rating: {rating}</h3>
   <p>Summary: {summary}</p>
   <p>Genera:{returnGenera()}</p>
+  {/* 1st Method */}
+
+  {/* <button>{age >= 18?"Watch Now": "Not Available"}</button> */} 
+      {/* 2nd Method */}
+  {/* <button>{canWatch}</button> */}
+        {/* 3rd Method */}
+
+          <button>{canWatch()}</button>
+
+
+
+
   </>
   )
 
